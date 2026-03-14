c674936901ddee275e69f5627f5f8476a835c7c6
imapoundhound <146208393+imapoundhound@users.noreply.github.com>
2026-03-14 19:18:02 -0400

    R2RWR

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // main.dart (excerpt)
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
 
 final _neonColorScheme = ColorScheme.fromSeed(
